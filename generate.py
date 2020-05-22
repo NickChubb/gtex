8d9bdf41f57ca998c627fae4563b5927c5f478f9
Nick Chubb <Nick.r.chubb@gmail.com>
2020-05-21 23:03:01 -0700

    Add more templates

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 #-----generate.py-----#
 #
-# Thi   s is the main py
+# This is the main py
 # file that generates
 # the LaTeX files
 #
@@ -18,10 +18,6 @@ import pylatex
 
 from datetime import date
 
-#---Global Variables---#
-
-#More types: Formula Sheet, .........   ?
-
 def createFolder(projectData):
 
     projectTitle = projectData["title"]
@@ -35,11 +31,17 @@ def createFolder(projectData):
 
     infoFile.write("Type: " + projectData["type"] + "\n")
     infoFile.write("Author: " + projectData["author"] + "\n")
+    infoFile.write("Student Number: " + projectData["studentnum"] + "\n")
     infoFile.write("Date: " + projectData["date"] + "\n")
+    infoFile.write("School: " + projectData["school"] + "\n")
     infoFile.write("Course: " + projectData["course"] + "\n")
     infoFile.write("Prof: " + projectData["prof"] + "\n")
     infoFile.write("\n")
     infoFile.write("This file was generated automatically by G-TeX on " + date.today().strftime("%B %d, %Y"))
+
+    infoFile.close()
+
+    
 
 
 
@@ -59,16 +61,23 @@ def generate(projectData):
 #--------Main--------#
 def main():
 
-    projectData = {}
+    projectData = {"type": ""}
     
     print("Welcome to G-TeX v.1")
     print("ctrl-c if you would not like to create a LaTeX project file in your current directory")
     print("Current project types are: report (r), assignment (a), notes (n)")
 
-    projectData["type"] = input("Enter the type of project you want to make: ")
+    while projectData["type"] != "r" and projectData["type"] != "a" and projectData["type"] != "n":
+        projectData["type"] = input("Enter the type of project you want to make: ")
+
+        if projectData["type"] != "r" and projectData["type"] != "a" and projectData["type"] != "n":
+            print("ERROR: value must be either r, a, n or ctl-c to quit")
+    
     projectData["author"] = input("Enter your name: ")
+    projectData["studentnum"] = input("Enter your student number: ")
     projectData["title"] = input("Enter the title of the project: ")
     projectData["date"] = input("Enter the due date or current date: ")
+    projectData["university"] = input("Enter your school's name: ")
     projectData["course"] = input("Enter the course name: ")
     projectData["prof"] = input("Enter the professor's name: ")
 
